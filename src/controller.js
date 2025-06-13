c89aa6f7977817e0aae394a8fab33e99b397fb41
Juanse <juansesosa45@gmail.com>
2025-06-13 17:32:45 -0300

    anotaciones

--- src/controller.js
+++ src/controller.js
@@ -3,7 +3,7 @@ const Records = require("./records.model");
 // Importar fs para manejar los archivos
 const fs = require("fs");
 
-// readline para leer linea por linea sin cargar la memoria
+// Readline para leer linea por linea sin cargar la memoria
 const readline = require("readline");
 
 const upload = async (req, res) => {
@@ -13,27 +13,28 @@ const upload = async (req, res) => {
     return res.status(400).json({ message: "No file was uploaded" });
   }
   try {
-    // Se crea un steam para no sobrecargar la memoria
+    // Se crea un stream para no sobrecargar la memoria
     const stream = fs.createReadStream(file.path);
 
-    // cada vez que se lee una liena del archivo se captura aca
+    // cada vez que se lee una linea del archivo se captura
     const rl = readline.createInterface({ input: stream });
 
-    // Se verifica que sea la primera linea del archivo
+    // La primera linea del archivo no tiene informacion util
+    // Se verifica que estamos en la primera linea
     let isFirstLine = true;
-    // se hace la llamada a cada linea de forma asyncrona para no bloquar la memoria
+    // Se hace la llamada a cada linea de forma asincrona para no bloquear la memoria
     for await (const line of rl) {
-      // La Primera linea del archivo no se tiene que procesar
+      // La primera linea del archivo no se tiene que procesar
       if (isFirstLine) {
         isFirstLine = false;
         continue;
       }
 
-      // Se hace una destructuracion para tener todos los datos
+      // Se hace destructuring para tener todos los datos
       const [id, firstname, lastname, email, email2, profession] =
         line.split(",");
 
-      // Se crea el objeto new Record como un nuevo registro para la base de datos
+      // Se crea el objeto Record como un nuevo registro para la base de datos
       const newRecord = new Records({
         // En el esquema id esta declarado como un number
         id: Number(id),
@@ -43,9 +44,9 @@ const upload = async (req, res) => {
         email2,
         profession,
       });
-      // si new record se creo correctamente
+      // Si newRecord se creo correctamente
       if (newRecord) {
-        // se manda a guardar en la base de datos
+        // Se manda a guardar en la base de datos
         await newRecord.save();
       } else {
         throw new Error(`Failed to load user ${id}`);
